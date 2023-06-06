e076ced216e6abcdc651324b506e007de48e008e
Tangerie <josh.cavill01@gmail.com>
2023-06-06 13:02:30 +1000

    fixed frontend port

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,5 +1,16 @@
+import { useQuery } from "@modules/Util/Query"
+import { useCallback } from "preact/hooks";
+import { JSX } from "preact";
+
 export default function SearchBar() {
+    const query = useQuery();
+    const onChange = useCallback((ev : JSX.TargetedEvent<HTMLInputElement, Event>) => {
+        console.log(ev);
+    }, [query]);
+
     return <input type="text"
-        class="form-input rounded-lg text-xl w-full focus:ring-purple-600 focus:ring-opacity-50" 
+        class="form-input rounded-lg text-xl w-full focus:ring-purple-600 focus:ring-opacity-50"
+        value={query.get("q") ?? ""}
+        onChange={onChange}
     />
 }
