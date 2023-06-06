8936d350a3ac716ca41e02c8367ce52926d7a8a5
Tangerie <josh.cavill01@gmail.com>
2023-06-06 13:38:45 +1000

    removed build from api

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,16 +1,23 @@
 import { useQuery } from "@modules/Util/Query"
-import { useCallback } from "preact/hooks";
+import { useCallback, useRef, useState } from "preact/hooks";
 import { JSX } from "preact";
 
 export default function SearchBar() {
     const query = useQuery();
-    const onChange = useCallback((ev : JSX.TargetedEvent<HTMLInputElement, Event>) => {
-        console.log(ev);
-    }, [query]);
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    const onKeyDown = useCallback((ev : JSX.TargetedEvent<HTMLInputElement, Event>) => {
+        // @ts-ignore
+        if(ev.key === 'Enter') {
+            console.log("Submitting", inputRef.current!.value);
+        }
+    }, []);
 
     return <input type="text"
         class="form-input rounded-lg text-xl w-full focus:ring-purple-600 focus:ring-opacity-50"
         value={query.get("q") ?? ""}
-        onChange={onChange}
+        ref={inputRef}
+        onKeyDown={onKeyDown}
+        placeholder="Enter search term..."
     />
 }
