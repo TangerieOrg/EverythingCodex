79ee6816a309cdd06264f88c8870bf370a35b622
Tangerie <josh.cavill01@gmail.com>
2023-06-07 20:19:23 +1000

    still broken

--- api/src/routes/fake.ts
+++ api/src/routes/fake.ts
@@ -50,7 +50,7 @@ FakeGPTRoutes.post('/search', async (req, res) => {
     
     for(const item of EXAMPLE_SEARCH_RESULTS) {
         await sleep(750);
-        res.write(`[${req.body.term}] ` + item + '\n')
+        res.write(item + '\n')
     }
     res.end();
 })
@@ -77,7 +77,7 @@ FakeGPTRoutes.post('/related', async (req, res) => {
 
     for(const item of EXAMPLE_RELATED_RESULTS) {
         await sleep(750);
-        res.write(`[${req.body.title}] ` + item + '\n')
+        res.write(item + '\n')
     }
     res.end();
 })
@@ -108,8 +108,6 @@ function randomInteger(min : number, max : number) {
 
 FakeGPTRoutes.post('/view', async (req, res) => {
     res.statusCode = 200;
-
-    res.write(`${req.body.title}\n`);
 
     let i = 0;
     while(i < EXAMPLE_VIEW_PAGE.length) {
