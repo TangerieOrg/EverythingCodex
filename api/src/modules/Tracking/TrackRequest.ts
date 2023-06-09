e88f50a6b4e62e2d198903350ee4607595c8dd1e
Tangerie <josh.cavill01@gmail.com>
2023-06-09 23:01:14 +1000

    added larger tracking log

--- api/src/modules/Tracking/TrackRequest.ts
+++ api/src/modules/Tracking/TrackRequest.ts
@@ -1,8 +1,27 @@
 import { Request } from "express";
 
+export const recordToLargeLog = async (req : Request, result : any) => {
+    if(!await req.redis.exists("all")) {
+        await req.redis.json.set("all", ".", {
+            requests: []
+        });
+    }
+
+    await req.redis.json.arrAppend(
+        "all", 
+        ".requests", 
+        {
+            by: req.tracking,
+            path: req.path,
+            request: req.body,
+            result
+        }
+    );
+} 
+
 export const recordRequest = async (req : Request, result : any) => {
-    if(!await req.redis.exists(`${req.tracking}`)) {
-        await req.redis.json.set(`${req.tracking}`, ".", {
+    if(!await req.redis.exists(`users.${req.tracking}`)) {
+        await req.redis.json.set(`users.${req.tracking}`, ".", {
             requests: []
         });
     }
